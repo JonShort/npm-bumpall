bb3b3f37cfecccb3d91f8f39a4bb4cbf12753b50
JonShort <JonShort@users.noreply.github.com>
2023-08-29 11:10:07 +0100

    [util] - add thinking emoji constant

--- src/emojis.rs
+++ src/emojis.rs
@@ -4,4 +4,5 @@ pub const DIZZY: char = '\u{1F4AB}';
 pub const MAGNIFYING_GLASS: char = '\u{1F50D}';
 pub const POINT_RIGHT: char = '\u{1F449}';
 pub const ROCKET: char = '\u{1F680}';
+pub const THINKING: char = '\u{1F914}';
 pub const TROPHY: char = '\u{1F3C6}';
